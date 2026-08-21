12b05a080f6eeec07bb18f12f7704bfa6a8ea9fe
yourpov.dev <aalkinani222@gmail.com>
2026-08-21 12:43:35 -0400

    first commit

---
+++ include/config.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace cfg {
+    // process name to wait for, leave empty to choose on launch
+    inline constexpr const wchar_t* targetProcess = L""; // e.g., L"Valorant-Win64-Shipping.exe";
+
+    inline constexpr bool debugMode               = false;
